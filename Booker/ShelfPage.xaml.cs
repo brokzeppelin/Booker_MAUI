4347e5c6f5cd73e295c76d757489a59f2a471d22
ir h <brokzeppelin@gmail.com>
2024-10-24 21:06:33 +0200

    Added a method to get the path to the book.

--- Booker/ShelfPage.xaml.cs
+++ Booker/ShelfPage.xaml.cs
@@ -6,4 +6,36 @@ public partial class ShelfPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void AddToShelf(object sender, EventArgs e)
+    {
+        // TODO: null check
+        string pathToBook = await PickTheFile();
+
+        // TODO: Remove
+        booksList.Add(new Label { Text = "The book" });
+    }
+
+    private async Task<string?> PickTheFile()
+    {
+        // Define new file type for picking
+        var txtFileType = new FilePickerFileType(
+                  new Dictionary<DevicePlatform, IEnumerable<string>>
+                  {
+                       { DevicePlatform.iOS, new[] { "public.text" } }, // UTType values  
+                       { DevicePlatform.Android, new[] { "text/plain" } }, // MIME type  
+                       { DevicePlatform.WinUI, new[] { ".txt" } }, // file extension  
+                       { DevicePlatform.macOS, new[] { "txt" } },
+                  });
+
+        // Get the file
+        var txtPicked = await FilePicker.Default.PickAsync(new PickOptions
+        {
+            PickerTitle = "Pick a .txt file",
+            FileTypes = txtFileType
+        });
+
+        // Pass the path to file
+        return txtPicked?.FullPath;
+    }
 }
