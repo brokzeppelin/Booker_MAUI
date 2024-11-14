be3e7cc70cb3e756b04e96779f077354ed1c71f6
ir h <brokzeppelin@gmail.com>
2024-11-14 19:19:35 +0100

    + new class Filer to work file system operations

---
+++ Booker/Filer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booker
+{
+    public static class Filer
+    {
+        // New file type for picking
+        public static FilePickerFileType TxtFileType
+        {
+            get => GetFilePickerFileTypeTxt();
+        }
+
+        private static FilePickerFileType GetFilePickerFileTypeTxt()
+        {
+            return new FilePickerFileType(
+                        new Dictionary<DevicePlatform, IEnumerable<string>>
+                        {
+                       { DevicePlatform.iOS, new[] { "public.text" } }, // UTType values  
+                       { DevicePlatform.Android, new[] { "text/plain" } }, // MIME type  
+                       { DevicePlatform.WinUI, new[] { ".txt" } }, // file extension  
+                       { DevicePlatform.macOS, new[] { "txt" } },
+                        });
+        }
+
+        public static async Task<string> GetPickedFileFullPath()
+        {
+            var filePicked =  await FilePicker.Default.PickAsync(new PickOptions
+            {
+                PickerTitle = "Pick a .txt file",
+                FileTypes = TxtFileType
+            });
+
+            return filePicked?.FullPath ?? String.Empty;
+        }
+    }
+}
--- Booker/ShelfPage.xaml.cs
+++ Booker/ShelfPage.xaml.cs
@@ -20,36 +20,13 @@ public partial class ShelfPage : ContentPage
 
     private async void AddToShelf(object sender, EventArgs e)
     {
-        string pathToFile = await PickTheFile();
+        string pathToFile = await Filer.GetPickedFileFullPath();
         string userDir = FileSystem.Current.AppDataDirectory + "/UserBooks";
 
         if (pathToFile != null)
         {
             ProcessAndSave(pathToFile, userDir);
         }
-    }
-
-    private async Task<string?> PickTheFile()
-    {
-        // Define new file type for picking
-        var txtFileType = new FilePickerFileType(
-                  new Dictionary<DevicePlatform, IEnumerable<string>>
-                  {
-                       { DevicePlatform.iOS, new[] { "public.text" } }, // UTType values  
-                       { DevicePlatform.Android, new[] { "text/plain" } }, // MIME type  
-                       { DevicePlatform.WinUI, new[] { ".txt" } }, // file extension  
-                       { DevicePlatform.macOS, new[] { "txt" } },
-                  });
-
-        // Get the file
-        var txtPicked = await FilePicker.Default.PickAsync(new PickOptions
-        {
-            PickerTitle = "Pick a .txt file",
-            FileTypes = txtFileType
-        });
-
-        // Pass the path to file
-        return txtPicked?.FullPath;
     }
 
     private void InsertSettingsXML(string userFolder, string fileName)
