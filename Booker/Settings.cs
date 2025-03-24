b36aab64a3bec4f9eb88e4b273a7999a2a03210c
ir h <brokzeppelin@gmail.com>
2025-03-24 17:13:19 +0100

    Add model for SettingsPage

---
+++ Booker/Settings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booker
+{
+    public abstract class Setting    
+    {
+        public string Name { get; set; } = String.Empty;
+        public string Alias { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public int Value { get; set; }
+    }
+
+    public class SwitchSetting : Setting
+    {
+        
+    }
+
+    public class StepperSetting : Setting
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public int Step { get; set; }
+    }
+
+    public class SettingTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate SwitchSettingTemplate { get; set; }
+        public DataTemplate StepperSettingTemplate { get; set; }
+        public DataTemplate NoControlSettingTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            switch (item)
+            {
+                case SwitchSetting sw:
+                    return SwitchSettingTemplate;
+                case StepperSetting st:
+                    return StepperSettingTemplate;
+                default:
+                    return NoControlSettingTemplate;
+            }
+        }
+    }
+}
---
+++ Booker/ViewModel/SettingViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booker.ViewModel
+{
+    public class SettingViewModel
+    {
+        public ObservableCollection<Setting> Settings { get; set; }
+
+        public SettingViewModel()
+        {
+            Settings = new ObservableCollection<Setting> {
+                new SwitchSetting() { 
+                    Alias = "DarkTheme", 
+                    Name = "Dark theme", 
+                    Value = 0, 
+                    Description = "Choose color scheme for the app" 
+                },
+
+                new SwitchSetting() { 
+                    Alias = "DailyReadingDose", 
+                    Name = "Daily reading dose", 
+                    Value = 0, 
+                    Description = "Track your daily reading progress" 
+                },
+
+                new StepperSetting() { 
+                    Alias = "PagesPerDay", 
+                    Name = "Pages per day", 
+                    Value = 20, 
+                    Description = "Set a daily page goal for your reading",
+                    Min = 1,
+                    Max = 1000,
+                    Step = 1
+                },
+
+                new SwitchSetting() { 
+                    Alias = "EnableReadingGoalNotifications", 
+                    Name = "Enable notifications when the reading goal is reached", 
+                    Value = 0 
+                },
+
+                new StepperSetting() { 
+                    Alias = "FontSize", 
+                    Name = "Font size", 
+                    Value = 18, 
+                    Description = "Set the font size for the book",
+                    Min = 5,
+                    Max = 35,
+                    Step = 1
+                },
+
+                new SwitchSetting() { 
+                    Alias = "BoldText", 
+                    Name = "Bold text", 
+                    Value = 0, 
+                    Description = "Adjust the font weight for the book" 
+                }
+            };
+        }
+    }
+}
